687c4766261f7c9a41ea2c23342b8c74d7889a69
Gergő Papp <imgergopapp@gmail.com>
2019-04-03 20:43:01 +0200

    Added Task1 Dao

---
+++ src/main/java/com/codecool/web/dao/Task1Dao.java
@@ -0,0 +1,12 @@
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Task1Result;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Task1Dao {
+
+    List<Task1Result> findAll() throws SQLException;
+
+}
---
+++ src/main/java/com/codecool/web/dao/database/DatabaseTask1Dao.java
@@ -0,0 +1,45 @@
+package com.codecool.web.dao.database;
+
+import com.codecool.web.dao.Task1Dao;
+import com.codecool.web.model.Task1Result;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseTask1Dao extends AbstractDao implements Task1Dao {
+
+
+    public DatabaseTask1Dao(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public List<Task1Result> findAll() throws SQLException {
+
+        List<Task1Result> task1Results = new ArrayList<>();
+
+        //Task 1 answer
+        String sql = "SELECT product_name AS Product, company_name AS Company FROM products " +
+            "INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id " +
+            "ORDER BY product, company;";
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                task1Results.add(fetchResult(resultSet));
+            }
+        }
+        return task1Results;
+    }
+
+    private Task1Result fetchResult(ResultSet resultSet) throws SQLException {
+        String company = resultSet.getString("Company");
+        String product = resultSet.getString("Product");
+        Task1Result test = new Task1Result(product,company);
+        return test;
+    }
+}
