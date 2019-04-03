aea1c5adaebb0ae984e7fdf324806cf2ead2c641
Gergő Papp <imgergopapp@gmail.com>
2019-04-03 20:38:48 +0200

    Added Task1Result Constructor & Getters

--- src/main/java/com/codecool/web/model/Task1Result.java
+++ src/main/java/com/codecool/web/model/Task1Result.java
@@ -1,4 +1,21 @@
 package com.codecool.web.model;
 
 public class Task1Result {
+    private String product;
+    private String company;
+
+    public Task1Result(String product, String company) {
+        this.product = product;
+        this.company = company;
+
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
 }
