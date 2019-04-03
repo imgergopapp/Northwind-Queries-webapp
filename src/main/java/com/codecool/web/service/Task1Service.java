e9aa416a26e0a90d1d06675f22bf0bbaf8e98283
Gergő Papp <imgergopapp@gmail.com>
2019-04-03 20:43:18 +0200

    Added Task1Service interface

---
+++ src/main/java/com/codecool/web/service/Task1Service.java
@@ -0,0 +1,10 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Task1Result;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Task1Service {
+    List<Task1Result> getResults() throws SQLException;
+}
