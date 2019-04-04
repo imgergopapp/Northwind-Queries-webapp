d9ebcd0b6c4b5de990c3023099f908f14c0685af
Gergő Papp <imgergopapp@gmail.com>
2019-04-04 12:31:55 +0200

    Added InvalidFormException

---
+++ src/main/java/com/codecool/web/service/exception/InvalidFormException.java
@@ -0,0 +1,8 @@
+package com.codecool.web.service.exception;
+
+public class InvalidFormException extends RuntimeException{
+
+    public InvalidFormException(String message) {
+        super(message);
+    }
+}
